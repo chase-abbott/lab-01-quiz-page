0238e1cac3fd226e6d688525c3d12ddc89e7b0f1
chase-abbott <cabbott93@gmail.com>
2021-03-23 16:17:05 -0700

    add ReadMe, basic html

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,16 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { isYes } from './utils.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    const expected = true;
+    // const actual = isYes(someString);
+
+    //expect.equal(actual, expected);
+    expect.equal(isYes('YES'), expected);
+    expect.equal(isYes('yes'), expected);
+    expect.equal(isYes('nah'), !expected);
 });
